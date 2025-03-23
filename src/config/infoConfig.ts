a9dcbfe9da99a701cbe1da02778e74f56af5973e
libokai <li_bo_kai@qq.com>
2025-03-23 14:32:49 +0800

    基本配置信息更改

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,11 +3,11 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = '黎博凯Keaton Li'
+export const headline = 'SoftWare Engineer at Shenzhen.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
+export const email = 'li_bo_kai@qq.com'
 export const githubUsername = 'derekhut'
 
 // about page
