4a66cf9bb190aba7f82160b86bf2601e17d26025
libokai <li_bo_kai@qq.com>
2025-03-24 10:44:23 +0800

    feat: 工作经历更新

--- src/config/career.ts
+++ src/config/career.ts
@@ -10,31 +10,24 @@ export type CareerItemType = {
   
 export const careerList: Array<CareerItemType> = [
     {
-      company: 'Shanghai Soong Ching Ling School',
-      title: 'Computer Science Teacher',
-      logo: 'college',
-      start: '2020',
-      end: 'Present'
-    },
-    {
-      company: 'Bigo Live',
-      title: 'US Operations',
+      company: '名通科技有限公司',
+      title: 'Java 开发工程师',
       logo: 'coffee',
-      start: '2018',
-      end: '2020'
+      start: '2025-03',
+      end: 'up to now'
     },
     {
-      company: 'Sinovation Ventures',
-      title: 'Investment Analyst',
+      company: '华阳国际城市科技有限公司',
+      title: '初级Java研发工程师',
       logo: 'bank',
-      start: '2017',
-      end: '2018'
+      start: '2023-07',
+      end: '2024-12'
     },
     {
-      company: 'Expedia',
-      title: 'Software Engineer',
+      company: '上海云砺信息科技有限公司',
+      title: 'Java 开发实习生',
       logo: 'coffee',
-      start: '2015',
-      end: '2017'
+      start: '2022-11',
+      end: '2023-05'
     }
   ]
